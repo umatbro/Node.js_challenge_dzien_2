4243f7fd1aefe56bb9643453a3c2925ef19e1ff8
umatbro <umatbroo@gmail.com>
2018-02-22 00:01:34 +0100

    zad2

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,19 @@
-//Twój kod
+let fs = require('fs');
+
+let dataDir = `${__dirname}/data/zadanie02`;
+fs.readdir(dataDir, (err, listOfFiles) => {
+   if (err) console.log(`Error reading ${dataDir}`, err);
+   else {
+       for (let file of listOfFiles) {
+           // read file and print its contents
+           fs.readFile(`${dataDir}/${file}`, 'utf-8', (err, content) => {
+               console.log(`===== ${file} =====`);
+               if (err) console.log(`Error reading file ${file}`, err);
+               else {
+                   console.log(content);
+               }
+               console.log(`------------------`)
+           });
+       }
+   }
+});
