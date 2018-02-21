e4cccc94d506acfe553aacc59ba53b3eea08f47e
umatbro <umatbroo@gmail.com>
2018-02-21 23:45:31 +0100

    zad1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile(`${__dirname}/data/zadanie01/input.json`, 'utf-8', (err, data) => {
+   if (err) console.log(`Error reading the file: ${err}`);
+   else {
+       let numbers = JSON.parse(data);
+       let sum = numbers.reduce((acc, num) => acc + num);
+
+       // save sum to file
+       fs.writeFile(`${__dirname}/data/zadanie01/sum.txt`, sum, err => {
+           if (err) console.log(`Error saving data to file: ${err}`);
+           else {
+               console.log(`Successfully saved sum to file (sum = ${sum})`);
+           }
+       })
+   }
+});
